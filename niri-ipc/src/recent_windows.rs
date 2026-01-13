58f292137b86b55c049ed1adf5a5ea628b21692d
Yaroslav Bolyukin <iam@lach.pw>
2026-01-13 23:46:56 +0100

    docs: stub docs for new ipc actions

--- niri-ipc/src/recent_windows.rs
+++ niri-ipc/src/recent_windows.rs
@@ -1,5 +1,9 @@
+//! When the focused window is closed, some other window should be focused instead
+//! This module describes selection of such next window.
+
 use serde::{Deserialize, Serialize};
 
+/// IDK
 #[derive(Serialize, Deserialize, Clone, Copy, Debug, Default, PartialEq, Eq)]
 #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
 #[cfg_attr(feature = "json-schema", derive(schemars::JsonSchema))]
@@ -11,6 +15,8 @@ pub enum MruDirection {
     Backward,
 }
 
+/// When choosing a window to switch after closing the other window, which window scope should we
+/// look up first
 #[derive(Serialize, Deserialize, Clone, Copy, Debug, Default, PartialEq, Eq)]
 #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
 #[cfg_attr(feature = "knus", derive(knus::DecodeScalar))]
@@ -25,6 +31,8 @@ pub enum MruScope {
     Workspace,
 }
 
+/// When choosing a window to switch after closing the other window, which window should we look
+/// up first
 #[derive(Serialize, Deserialize, Clone, Copy, Debug, Default, PartialEq, Eq)]
 #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
 #[cfg_attr(feature = "knus", derive(knus::DecodeScalar))]
