a27a37744c057cbab5fe14cc91ef6c4daf98a958
Naxdy <naxdy@naxdy.org>
2026-02-21 13:32:37 +0100

    feat(nix): correctly pre-configure XDG portals

--- nix/modules/niri-nixos.nix
+++ nix/modules/niri-nixos.nix
@@ -2,6 +2,8 @@
   overlay,
 }:
 {
+  config,
+  lib,
   pkgs,
   ...
 }:
@@ -11,4 +13,24 @@
   ];
 
   programs.niri.package = pkgs.niriPackages.niri;
+
+  xdg.portal = lib.mkIf config.programs.niri.enable {
+    enable = true;
+    xdgOpenUsePortal = lib.mkDefault true;
+    extraPortals = [
+      pkgs.kdePackages.xdg-desktop-portal-kde
+      pkgs.xdg-desktop-portal-gnome
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    config = {
+      kde = {
+        default = [
+          "kde"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.Screenshot" = "kde";
+        "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+      };
+    };
+  };
 }
